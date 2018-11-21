283462781b690b5c8661524f8bd94ad1aafe28fd
Johannes Leutgeb <johannes.leutgeb2002@gmail.com>
2018-11-21 17:42:56 +0100

    Projektdateien hinzufügen.

---
+++ Servomotor/Servomotor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Servomotor.ino
+ Created:	21.11.2018 17:42:52
+ Author:	Johannes
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
